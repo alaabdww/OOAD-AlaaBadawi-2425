deaa68c53f5809f6b014186ec76e720ed18031e4
alaabdww <91311739+alaabdww@users.noreply.github.com>
2025-05-20 14:11:44 +0200

    wpftaken

--- SlnLes01HerhalingAanvulling/WpfTaken/MainWindow.xaml.cs
+++ SlnLes01HerhalingAanvulling/WpfTaken/MainWindow.xaml.cs
@@ -25,38 +25,113 @@ namespace WpfTaken
             InitializeComponent();
         }
 
-        private void Checkform() 
+        private bool Checkform() 
         {
+            bool isValid = true;
+             ErrorTxt.Text = null;
             if (string.IsNullOrWhiteSpace(NaamTxt.Text))
             {
                 ErrorTxt.Text += "Gelieve een naam toe te voegen" + Environment.NewLine;
+                isValid = false;
             }
             if (PrioriteitCmbbx.SelectedIndex == 3)
             {
                 ErrorTxt.Text += "Gelieve een prioriteit te kiezen" + Environment.NewLine;
+                isValid = false;
             }
             if (Datumpicker.SelectedDate == null)
             {
                 ErrorTxt.Text += "Gelieve een datum te kiezen" + Environment.NewLine;
+                isValid = false;
             }
             if (radAdam.IsChecked == false && radBilal.IsChecked == false && radChelsey.IsChecked == false)
             {
                 ErrorTxt.Text += "Gelieve een verantwoordelijke te kiezen" + Environment.NewLine;
+                isValid = false;
             }
+
+            return isValid;
+
         }
+
+        Stack<ListBoxItem> verwijderdeItems = new Stack<ListBoxItem>();
 
         private void Toevoegen_Click(object sender, RoutedEventArgs e)
         {
-            Checkform();
-            
-            ErrorTxt.Text = null;
 
-           
-                ListBoxItem item = new ListBoxItem();
-                item.Content = $"{NaamTxt.Text} (deadline: {Datumpicker.SelectedDate}; door ";
 
-                Lstbx.Items.Add(item);
+            if (!Checkform()) 
+            {
+                return;
+            }
 
+            string uitvoerder;
+            if (radAdam.IsChecked == true)
+            {
+                uitvoerder = "Adam";
+            }
+            else if (radBilal.IsChecked == true)
+            {
+                uitvoerder = "Bilal";
+            }
+            else
+            {
+                uitvoerder = "Chelsey";
+            }
+
+            ListBoxItem item = new ListBoxItem();
+            item.Content = $"{NaamTxt.Text} (deadline: {Datumpicker.SelectedDate.Value.ToString("dd/MM/yyyy")}; door {uitvoerder})";
+            string prioriteit = ((ComboBoxItem)PrioriteitCmbbx.SelectedItem).Content.ToString();
+            switch (prioriteit)
+            {
+                case "Hoog":
+                    item.Background = Brushes.LightCoral;
+                    break;
+
+                case "Gemiddeld":
+                    item.Background = Brushes.LightYellow;
+                    break;
+
+                case "Laag":
+                    item.Background = Brushes.LightSeaGreen;
+                    break;
+            }
+
+            Lstbx.Items.Add(item);
+
+            NaamTxt.Text = "";
+            PrioriteitCmbbx.SelectedIndex = 3;
+            Datumpicker.SelectedDate = null;
+            radAdam.IsChecked = false;
+            radBilal.IsChecked = false;
+            radChelsey.IsChecked = false;
+        }
+
+        private void Lstbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            verwijderenBtn.IsEnabled = true;
+        }
+
+        private void verwijderenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem selectedItem = (ListBoxItem)Lstbx.SelectedItem;
+            if (selectedItem != null) 
+            {
+                verwijderdeItems.Push(selectedItem);
+                Lstbx.Items.Remove(selectedItem);
+            }
+
+            terugzettenBtn.IsEnabled = true;
+            verwijderenBtn.IsEnabled = false;
+        }
+
+        private void terugzettenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem item = verwijderdeItems.Pop();
+            Lstbx.Items.Add(item);
+
+            terugzettenBtn.IsEnabled = false;
+            verwijderenBtn.IsEnabled = true;
         }
     }
 }
